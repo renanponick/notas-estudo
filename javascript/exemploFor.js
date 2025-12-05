dd60fff11ff43403604f4c938e427cdd07a0762e
Renan <warreir@gmail.com>
2025-12-04 21:17:06 -0300

    feat: add while for

---
+++ javascript/exemploFor.js
@@ -0,0 +1,5 @@
+let i; // explicar escopo
+const numero = Number(prompt("Digite um número: "))
+for(i = 0;  i <= 10; i++) {
+    alert(i + " X " + numero + " = " + (numero*i))
+}
---
+++ javascript/exemploWhile.js
@@ -0,0 +1,10 @@
+
+let numero;
+
+// faça
+do {
+    numero = Number( prompt("Digite um número") )
+// enquanto (verdade)
+} while(numero != 7);
+
+alert("Parabains vuce assertou")
