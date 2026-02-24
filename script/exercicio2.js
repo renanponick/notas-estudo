122cc0f215f331f1716486bfe6397ccdd1881d4a
Renan P <ponickrenan@gmail.com>
2026-02-23 22:16:08 -0300

    feat: exercicio 1

---
+++ script/exercicio2.js
@@ -0,0 +1,13 @@
+// Identificar Variáveis - ValorHora e HorasTrabalhadas
+// Identificar funções para serem utilizadas - Alert, Prompt
+// Identificar a operação à ser realizada - multiplicação *
+
+// Entrada
+const valorHora = Number(prompt("Digite o valor da hora:"))
+const horasTrabalhadas = Number(prompt("Digite as horas trabalhadas:"))
+
+// Processamento
+const resultado = valorHora*horasTrabalhadas
+
+// Saida
+alert("Seu salario é: " + resultado)
