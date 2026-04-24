61a94f1f496b06cf78e762584607b7d2d13a1473
Renan P <ponickrenan@gmail.com>
2026-04-23 21:15:17 -0300

    fim

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,31 +2,43 @@ import { useState } from 'react';
 import './style.css';
 
 function SignUp() {
-    const [ nome, setNome ] = useState("batata")
-    // variaveis
-    // funções
+    const [ nome, setNome ] = useState("sadasd")
+    const [ email, setEmail ] = useState("a@sdas")
+    const [ senha, setSenha ] = useState("sadsa")
+    const [ resultado, setResultado ] = useState()
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(nome, email, senha)
+        setResultado(nome);
+    }
 
     return (
         <>
             <h2>Criar Conta</h2>
-
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Nome</label>
                 <input
                     type="text"
                     value={nome}
-                    onChange={ (event) => setNome(event.value) }
+                    onChange={ (event) => setNome(event.target.value) }
                 />
 
                 <label>Email</label>
                 <input
                     type="email"
+                    value={email}
+                    onChange={ (e) => setEmail(e.target.value) }
                 />
 
                 <label>Senha</label>
                 <input
                     type="password"
+                    value={senha}
+                    onChange={ (e) => setSenha(e.target.value) }
                 />
+
+                <p>{resultado}</p>
 
                 <button type="submit">Cadastrar</button>
             </form>
